775c28904b3378ff77b7f93933a0c5fe1128ea87
Mark Hämmerling <dev@markh.de>
2024-05-08 20:01:17 +0200

    Fix import

--- source/kivar_plugin.py
+++ source/kivar_plugin.py
@@ -2,7 +2,7 @@ from os import path as os_path
 import wx
 import wx.lib.agw.hyperlink as hyperlink
 import pcbnew
-from kivar_backend import build_fpdict, build_vardict, version, get_choice_dict, detect_current_choices, natural_sort_key, apply_selection, store_fpdict, uuid_to_fp, pcbnew_compatibility_error, legacy_expressions_found
+from .kivar_backend import build_fpdict, build_vardict, version, get_choice_dict, detect_current_choices, natural_sort_key, apply_selection, store_fpdict, uuid_to_fp, pcbnew_compatibility_error, legacy_expressions_found
 
 # TODO:
 #
