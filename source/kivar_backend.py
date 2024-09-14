1763e1d004ce98eb90709008cee26cfd827576a0
Mark Hämmerling <dev@markh.de>
2024-09-14 12:43:45 +0200

    Increase version number

--- source/kivar_backend.py
+++ source/kivar_backend.py
@@ -20,7 +20,7 @@ from copy import deepcopy
 # TODO more testing!
 
 def version():
-    return '0.3.9990'
+    return '0.4.0'
 
 def pcbnew_compatibility_error():
     ver = pcbnew.GetMajorMinorPatchVersion()
